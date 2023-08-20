9844b39968289c8af9d51847328a036fc86d0181
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-20 22:56:45 +0530

    first

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ let engine = Matter.Engine.create();
 
 let render = Matter.Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        wireframes: false
+    }
 });
 
-let ground = Matter.Bodies.rectangle(400,600,810,40, {isStatic: true});
-let box1 = Matter.Bodies.rectangle(400,200,80,80);
-let box2 = Matter.Bodies.rectangle(450,50,80,80);
+let ground = Matter.Bodies.rectangle(1200,600,400,50, {isStatic: true});
+
 
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint  = Matter.MouseConstraint.create(engine, {
@@ -20,7 +24,15 @@ let mouseConstraint  = Matter.MouseConstraint.create(engine, {
 
 render.mouse = mouse;
 
-Matter.World.add(engine.world, [box1, box2, ground, mouseConstraint]);
+let ball = Matter.Bodies.circle(300,600,20);
+let sling 
+
+let stack = Matter.Composites.stack(1100, 270, 6, 6, 0, 0, function(x,y) {
+    // let sides =  Math.round(Matter.Common.random(2,8));
+    return Matter.Bodies.polygon(x, y, 8, 30);
+});
+
+Matter.World.add(engine.world, [stack, ground, mouseConstraint]);
 Matter.Engine.run(engine);
 Matter.Render.run(render);
 
