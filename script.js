0f8b68e5bf578c16bfad4cabcc323260492e9d2f
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-23 14:25:36 +0530

    five

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let sling = Matter.Constraint.create({
     stiffness: 0.05
 });
 
-let stack = Matter.Composites.stack(1100, 270, 6, 6, 0, 0, function(x,y) {
+let stack = Matter.Composites.stack(1100, 270, 8, 8, 0, 0, function(x,y) {
     // let sides =  Math.round(Matter.Common.random(2,8));
     return Matter.Bodies.polygon(x, y, 8, 30);
 });
@@ -41,7 +41,7 @@ Matter.Events.on(mouseConstraint, 'enddrag', function(e) {
     if(e.body === ball) firing = true;
 });
 
-Matter.Events.on(engine, 'afterupdate', function() {
+Matter.Events.on(engine, 'afterUpdate', function() {
     if(firing && Math.abs(ball.position.x-300)< 20 && Math.abs(ball.position.y-600) < 20) {
         ball = Matter.Bodies.circle(300, 600, 20);
         Matter.World.add(engine.world, ball);
